400b51740988815ca40478e23b0892e25de460ce
gschumacher87-art <gschumacher87@gmail.com>
2026-03-25 18:13:44 +1100

    Create collectible.js

---
+++ collectible.js
@@ -0,0 +1,29 @@
+export class Collectible {
+  constructor(x, y, size = 20) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.collected = false;
+  }
+
+  draw(ctx) {
+    if (!this.collected) {
+      ctx.fillStyle = 'gold';
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.size / 2, 0, Math.PI * 2);
+      ctx.fill();
+    }
+  }
+
+  checkCollision(dragon) {
+    if (!this.collected &&
+        dragon.x < this.x + this.size &&
+        dragon.x + dragon.width > this.x &&
+        dragon.y < this.y + this.size &&
+        dragon.y + dragon.height > this.y) {
+      this.collected = true;
+      return true;
+    }
+    return false;
+  }
+}
