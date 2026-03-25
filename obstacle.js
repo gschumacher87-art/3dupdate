92e711a03106bdfe4d575e166447df7d9bebc9eb
gschumacher87-art <gschumacher87@gmail.com>
2026-03-25 19:04:37 +1100

    Update obstacle.js

--- obstacle.js
+++ obstacle.js
@@ -2,28 +2,43 @@ export class Pipe {
   constructor(x, topHeight, gapHeight = 180, width = 60) {
     this.x = x;
     this.topHeight = topHeight;
-    this.gapHeight = gapHeight; // now correctly passed from app.js
+    this.gapHeight = gapHeight;
     this.width = width;
     this.scored = false;
   }
 
-  move(speed = 2) { // slower = matches snake pacing
+  move(speed = 2) { // slower = snake style
     this.x -= speed;
   }
 
   draw(ctx, canvasHeight) {
-    ctx.fillStyle = 'red';
+    // --- Top pipe ---
+    // Pipe lip
+    ctx.fillStyle = '#4d0000';
+    ctx.fillRect(this.x - 5, this.topHeight - 20, this.width + 10, 20);
 
-    // Top pipe
+    // Main pipe
+    ctx.fillStyle = '#ff0000';
     ctx.fillRect(this.x, 0, this.width, this.topHeight);
 
-    // Bottom pipe
-    ctx.fillRect(
-      this.x,
-      this.topHeight + this.gapHeight,
-      this.width,
-      canvasHeight - (this.topHeight + this.gapHeight)
-    );
+    // Top pipe shading
+    ctx.fillStyle = '#8B0000';
+    ctx.fillRect(this.x, 0, 5, this.topHeight); // left edge
+    ctx.fillRect(this.x + this.width - 5, 0, 5, this.topHeight); // right edge
+
+    // --- Bottom pipe ---
+    // Pipe lip
+    ctx.fillStyle = '#4d0000';
+    ctx.fillRect(this.x - 5, this.topHeight + this.gapHeight, this.width + 10, 20);
+
+    // Main pipe
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(this.x, this.topHeight + this.gapHeight, this.width, canvasHeight - (this.topHeight + this.gapHeight));
+
+    // Bottom pipe shading
+    ctx.fillStyle = '#8B0000';
+    ctx.fillRect(this.x, this.topHeight + this.gapHeight, 5, canvasHeight - (this.topHeight + this.gapHeight)); // left edge
+    ctx.fillRect(this.x + this.width - 5, this.topHeight + this.gapHeight, 5, canvasHeight - (this.topHeight + this.gapHeight)); // right edge
   }
 
   checkCollision(dragon) {
