8b8c08924f32b820d74f22aa8971d0d2af545506
gschumacher87-art <gschumacher87@gmail.com>
2026-03-25 18:14:35 +1100

    Create obstacle.js

---
+++ obstacle.js
@@ -0,0 +1,25 @@
+export class Pipe {
+  constructor(x, topHeight, width = 60, gapHeight = 150) {
+    this.x = x;
+    this.topHeight = topHeight;
+    this.width = width;
+    this.gapHeight = gapHeight;
+    this.scored = false;
+  }
+
+  move(speed = 3) {
+    this.x -= speed;
+  }
+
+  draw(ctx, canvasHeight) {
+    ctx.fillStyle = '#ff0000';
+    ctx.fillRect(this.x, 0, this.width, this.topHeight);
+    ctx.fillRect(this.x, this.topHeight + this.gapHeight, this.width, canvasHeight - this.topHeight - this.gapHeight);
+  }
+
+  checkCollision(dragon) {
+    return dragon.x + dragon.width > this.x &&
+           dragon.x < this.x + this.width &&
+           (dragon.y < this.topHeight || dragon.y + dragon.height > this.topHeight + this.gapHeight);
+  }
+}
