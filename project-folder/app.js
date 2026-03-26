b298460a2ca6b8781449eb8a5a1e01bbbe87cdda
gschumacher87-art <gschumacher87@gmail.com>
2026-03-26 17:14:32 +1100

    Update app.js

--- project-folder/app.js
+++ project-folder/app.js
@@ -42,7 +42,7 @@ dragon.onload = function () {
     // Manual offsets to keep body visually centered
     const frameOffsets = [
         { x: -30, y: 0 },   // frame 0
-        { x: -20, y: 0 }, // frame 1 (wings right)
+        { x: -5, y: 0 }, // frame 1 (wings right)
         { x: -10, y: 0 }    // frame 2 (wings left)
     ];
 
