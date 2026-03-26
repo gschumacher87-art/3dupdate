911973080e3f4e70889c71c8ae5dc3ec88a7aa00
gschumacher87-art <gschumacher87@gmail.com>
2026-03-26 21:57:43 +1100

    Update app.js

--- project-folder/app.js
+++ project-folder/app.js
@@ -4,6 +4,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// TEST DRAW (proves canvas works)
-ctx.fillStyle = "red";
-ctx.fillRect(50, 50, 200, 200);
+const dragon = new Image();
+dragon.src = './dragon.png';
+
+dragon.onload = () => {
+  ctx.drawImage(dragon, 100, 100, 150, 150);
+};
+
+dragon.onerror = () => {
+  console.log("IMAGE NOT FOUND");
+};
