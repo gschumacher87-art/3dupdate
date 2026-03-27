8e5b1bd174f46c2f237ad784caa89ee14cc014ce
gschumacher87-art <gschumacher87@gmail.com>
2026-03-27 21:12:17 +1100

    Update app.js

--- project-folder/app.js
+++ project-folder/app.js
@@ -34,7 +34,7 @@ let tick = 0;
 const speed = 60;
 
 // 🔧 MANUAL FRAME OFFSETS (YOU TUNE THESE)
-const frameOffsetX = [12, 2, 15];
+const frameOffsetX = [20, 2, 7];
 const frameOffsetY = [0, 0, 0];
 
 // ===== PHYSICS =====
